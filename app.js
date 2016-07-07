86211ae0d2f1fa82c390aae4b25416051078fe72
Samantha Bretous <sam@samanthabretous.com>
2016-07-07 10:25:18 -0400

    initialized index, new and create routes

---
+++ app.js
@@ -0,0 +1,57 @@
+var bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    express     = require("express"),
+    app         = express()
+//APP config    
+mongoose.connect("mongodb://localhost/blog_app")
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}))
+
+//MONGOOSE/MODEL config
+var blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {type: Date, default: Date.now}
+})
+var Blog = mongoose.model("Blog", blogSchema)
+
+
+//RESTFUL ROUTES
+app.get("/", function(req, res){
+  res.redirect("/blogs")
+})
+
+//INDEX ROUTE
+app.get("/blogs", function(req, res){
+  Blog.find({}, function(err, blogs){
+    if(err){
+      console.log("error")
+    } else {
+      res.render("index", {blogs: blogs})      
+    }
+  })
+})
+
+//NEW ROUTE
+app.get("/blogs/new", function(req, res){
+  res.render("new")
+})
+
+//CREATE ROUTE
+app.post("/blogs", function(req, res){
+  //create blog
+  Blog.create(req.body.blog, function(err, newBlog){
+    if(err){
+      res.render("new")
+    } else {
+      //then redirect to the index
+      res.redirect("/blogs")
+    }
+  })
+})
+
+app.listen(process.env.PORT, process.env.IP, function(){
+  console.log("BLOG SERVER IS RUNNING")
+})
